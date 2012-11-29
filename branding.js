6a031881f50429137b68a5d89c065bf20932bc94
Bill Mills <mills.wj@gmail.com>
2012-11-29 14:01:18 -0800

    integrated navbar styling into header banner

--- branding.js
+++ branding.js
@@ -1,14 +1,17 @@
 //Generates branding
 
-function Banner(canvas) {
+function Banner(canvas, location, navbar, title) {
 	'use strict'
 
     //fetch canvas
     var canvas = document.getElementById(canvas);
     var context = canvas.getContext('2d');
 
+    //Choose rendering width: use 1200px or the actual width, whichever is larger:
+    var renderWidth = Math.max(1200, window.innerWidth);
+
     //resize canvas
-    context.canvas.width = window.innerWidth-10;
+    context.canvas.width = renderWidth-10;
     context.canvas.height = window.innerHeight*0.1;
 
     //style canvas
@@ -21,7 +24,7 @@ function Banner(canvas) {
 	context.moveTo(5,0);
 	context.lineTo(5,context.canvas.height*0.7);
 	context.arcTo(5,context.canvas.height*0.9,25,context.canvas.height*0.9,20);
-	context.lineTo(window.innerWidth-50,context.canvas.height*0.9);
+	context.lineTo(renderWidth-50,context.canvas.height*0.9);
 	context.stroke();
 
     //insert logo
@@ -34,4 +37,15 @@ function Banner(canvas) {
     //write title
     context.font= context.canvas.height*0.75+'px Times New Roman';
     context.fillText('GRIFFIN', context.canvas.height*0.7*.9/.8+20, 0.65*context.canvas.height);
+
+    //set navbar width:
+    var navigation = document.getElementById(navbar);
+    $(navigation).css('width', renderWidth-50);
+
+    //move local title around:
+    context.font = '24px Times New Roman'
+    var urHere = document.getElementById(location);
+    $(urHere).css('width', context.measureText(title).width);
+    $(urHere).css('left', renderWidth - 50 - context.measureText(title).width);
+    urHere.innerHTML = title;
 }
