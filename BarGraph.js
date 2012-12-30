835d2b8ab31fad7da70bb49d84695b50396edf5c
Bill Mills <mills.wj@gmail.com>
2012-12-30 13:47:49 -0800

    implemented BarGraph decorations

--- BarGraph.js
+++ BarGraph.js
@@ -1,4 +1,4 @@
-function BarGraph(cvas, nBars, barTitles, scaleMin, scaleMax){
+function BarGraph(cvas, nBars, title, barTitles, scaleMin, scaleMax, masterWaffle){
 
 	//bar chart levels:
 	this.oldLevels = [];
@@ -8,13 +8,26 @@ function BarGraph(cvas, nBars, barTitles, scaleMin, scaleMax){
 	this.scaleMin = scaleMin;
 	this.scaleMax = scaleMax;
 
+	//number of y-axis scale ticks:
+	this.yAxisTicks = 6;
+
+	//waffle canvas ID of which these meters are a subset:
+	this.masterWaffle = masterWaffle;
+
+	//chart title:
+	this.title = title;
+
     //fetch canvas:
     this.canvas = document.getElementById(cvas);
     this.context = this.canvas.getContext('2d');
 
     //canvas dimensions:
-    this.width = $('#'+cvas).attr('width');
-    this.height = $('#'+cvas).attr('height');
+    this.width = masterWaffle.totalWidth;
+    this.height = masterWaffle.totalHeight;
+    this.headerHeight = masterWaffle.headerHeight;
+    $('#'+cvas).attr('width', this.width);
+    $('#'+cvas).attr('height', this.height);
+    $(document.getElementById(cvas)).css('top', this.headerHeight);
 
     //number of bars:
     this.nBars = nBars;
@@ -24,8 +37,11 @@ function BarGraph(cvas, nBars, barTitles, scaleMin, scaleMax){
 
     //bar width:
     this.barWidth = this.width*0.8 / (1.05*nBars);
+    //vertical margins:
+    this.topMargin = 0.1*this.height;
+    this.bottomMargin = 0.2*this.height;
     //bar max height:
-    this.barMax = this.height*0.8;
+    this.barMax = this.height - this.topMargin - this.bottomMargin;
 
     //animation parameters:
     this.duration = 1; //seconds
@@ -53,7 +69,7 @@ function BarGraph(cvas, nBars, barTitles, scaleMin, scaleMax){
 		//loop over bars:
 		for(i=0; i<this.nBars; i++){
 			barHeight = this.oldLevels[i]*this.barMax + (this.levels[i] - this.oldLevels[i])*this.barMax*frame/this.nFrames;
-			barTop = this.height*0.9 - barHeight;
+			barTop = this.height - this.bottomMargin - barHeight;
 			this.context.fillRect(leftEdge, barTop, this.barWidth, barHeight);
 			leftEdge += 1.05*this.barWidth;
 		}
@@ -87,7 +103,7 @@ function BarGraph(cvas, nBars, barTitles, scaleMin, scaleMax){
 		for(var i=0; i<this.nBars; i++){
 			this.oldLevels[i] = this.levels[i];
 			this.levels[i] = (data[i] - this.scaleMin) / (this.scaleMax - this.scaleMin);
-			if(this.levels[i] <= 0) this.levels[i] = 0.01;
+			if(this.levels[i] <= 0.01) this.levels[i] = 0.01;
 			if(this.levels[i] > 1) this.levels[i] = 1;
 		}
 
@@ -99,22 +115,38 @@ function BarGraph(cvas, nBars, barTitles, scaleMin, scaleMax){
 
 		var i = 0;
 
+		//set label font:
+		this.context.font=0.25*this.barWidth+"px 'Raleway'";
+
 		//draw principle axes:
 		this.context.beginPath();
-		this.context.moveTo(this.width*0.9, this.height*0.9);
-		this.context.lineTo(this.width*0.1, this.height*0.9);
-		this.context.lineTo(this.width*0.1, this.height*0.1);
+		this.context.moveTo(this.width*0.9, this.height - this.bottomMargin);
+		this.context.lineTo(this.width*0.1, this.height - this.bottomMargin);
+		this.context.lineTo(this.width*0.1, this.topMargin);
 		this.context.stroke();
 
 		//draw x-axis labels:
 		for(i=0; i<this.nBars; i++){
 			this.context.save();
-			this.context.translate(this.width*0.1+(i+0.5)*1.05*this.barWidth,this.height*0.92);
+			this.context.translate(this.width*0.1+(i+0.5)*1.05*this.barWidth,this.height - 0.9*this.bottomMargin);
 			this.context.rotate(-Math.PI/2.4);
 			this.context.textAlign = 'right';
 			this.context.fillText(this.channelNames[i], 0, 0);
 			this.context.restore();
 		}
+
+		//draw y-axis ticks and labels:
+		for(i=0; i<this.yAxisTicks; i++){
+			this.context.beginPath();
+			this.context.moveTo(this.width*0.1, this.height - this.bottomMargin - i*(this.height - this.topMargin - this.bottomMargin)/(this.yAxisTicks-1) );
+			this.context.lineTo(this.width*0.1 - 10, this.height - this.bottomMargin - i*(this.height - this.topMargin - this.bottomMargin)/(this.yAxisTicks-1) );
+			this.context.stroke();
+			this.context.fillText( ((this.scaleMax-this.scaleMin)/(this.yAxisTicks-1)*i).toFixed(1),  this.width*0.1 - 10 - 20, this.height - this.bottomMargin - i*(this.height - this.topMargin - this.bottomMargin)/(this.yAxisTicks-1) + 5);
+		}
+
+		//draw chart title:
+		this.context.font=0.7*this.barWidth+"px 'Raleway'";
+		this.context.fillText(this.title, this.width*0.9 - this.context.measureText(this.title).width, this.height - 0.35*this.bottomMargin);
 
 	};
 
