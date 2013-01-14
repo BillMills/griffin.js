c0e5ebbffc1e555b95b142b94878856808f1d9ce
Bill Mills <mills.wj@gmail.com>
2013-01-14 14:09:43 -0800

    changed the data fetching template to make the minimal amount of ODB calls

--- masterControl.js
+++ masterControl.js
@@ -4,29 +4,40 @@ function masterLoop(rows, cols, ODBkeys, demandVoltage, reportVoltage, reportCur
     	waffle.update(demandVoltage, reportVoltage, reportCurrent, demandVramp, reportTemperature, alarmStatus, channelMask, rampStatus, callMyself);
     	barchart.update([ reportVoltage[0][0], reportVoltage[1][0], reportVoltage[2][0], reportVoltage[3][0], reportVoltage[4][0], reportVoltage[5][0], reportVoltage[6][0], reportVoltage[7][0], reportVoltage[8][0], reportVoltage[9][0], reportVoltage[10][0], reportVoltage[11][0] ], [alarmStatus[0][0], alarmStatus[1][0], alarmStatus[2][0], alarmStatus[3][0], alarmStatus[4][0], alarmStatus[5][0], alarmStatus[6][0], alarmStatus[7][0], alarmStatus[8][0], alarmStatus[9][0], alarmStatus[10][0], alarmStatus[11][0] ] );
     }
-    setTimeout(function(){masterLoop(rows, cols, ODBkeys, demandVoltage, reportVoltage, reportCurrent, demandVramp, reportTemperature, channelMask, alarmStatus, rampStatus, alarmTripLevel, scaleMax, waffle, barchart, 1)}, 60000);
+    setTimeout(function(){masterLoop(rows, cols, ODBkeys, demandVoltage, reportVoltage, reportCurrent, demandVramp, reportTemperature, channelMask, alarmStatus, rampStatus, alarmTripLevel, scaleMax, waffle, barchart, 1)}, 3000);
 }
 
 //populate rows by cols arrays with the appropriate information:
 function fetchNewData(rows, cols, ODBkeys, demandVoltage, reportVoltage, reportCurrent, demandVramp, reportTemperature, channelMask, alarmStatus, rampStatus, alarmTripLevel, scaleMax){
 
-    var testParameter;
-
-    //populate new data:
-    for(var i=0; i<rows; i++){
-        for(var j=0; j<cols; j++){
-            /*commented out for offline demo
-            var ODBindex = getMIDASindex(rows, cols);
-            demandVoltage[i][j] = ODBGet(ODBkeys[0]+'['+ODBindex+']');
-            reportVoltage[i][j] = ODBGet(ODBkeys[1]+'['+ODBindex+']');
-            reportCurrent[i][j] = ODBGet(ODBkeys[2]+'['+ODBindex+']');
-            demandVramp[i][j] = ODBGet(ODBkeys[3]+'['+ODBindex+']');
-            reportTemperature[i][j] = ODBGet(ODBkeys[4]+'['+ODBindex+']');
-            channelMask[i][j] = ODBGet(ODBkeys[5]+'['+ODBindex+']');
-            rampStatus[i][j] = ODBGet(ODBkeys[6]+'['+ODBindex+']');
-            //insert some logic here to interpret whatever came out of ODB into rampStatus as 1 for ramping and 0 for not ramping.
+    var testParameter, i, j, ODBindex;
+/*
+    //batch fetch all in one big lump:
+    var variablesRecord = ODBGetRecord(ODBkeys[0]);
+    var settingsRecord  = ODBGetRecord(ODBkeys[1]);
+    
+    var reqVoltage      = ODBExtractRecord(variablesRecord, ODBkeys[2]);
+    var measVoltage     = ODBExtractRecord(variablesRecord, ODBkeys[3]);
+    var measCurrent     = ODBExtractRecord(variablesRecord, ODBkeys[4]);
+    var rampUp          = ODBExtractRecord(settingsRecord,  ODBkeys[5]);
+    var measTemperature = ODBExtractRecord(variablesRecord, ODBkeys[6]);
+    var repoChState     = ODBExtractRecord(settingsRecord,  ODBkeys[7]);
+    var repoChStatus    = ODBExtractRecord(variablesRecord, ODBkeys[8]);
+*/          
+    for(i=0; i<rows; i++){
+        for(j=0; j<cols; j++){
+            /*
+            ODBindex = getMIDASindex(i, j);
+            demandVoltage[i][j]     = reqVoltage[ODBindex];
+            reportVoltage[i][j]     = measVoltage[ODBindex];   
+            reportCurrent[i][j]     = measCurrent[ODBindex];
+            demandVramp[i][j]       = rampUp[ODBindex];
+            reportTemperature[i][j] = measTemperature[ODBindex];
+            channelMask[i][j]       = repoChState[ODBindex];
+            rampStatus[i][j]        = repoChStatus[ODBindex];
+            if(rampStatus[i][j] == 3 || rampStatus[i][j] == 5) rampStatus[i][j] = 1;
+            else rampStatus[i][j] = 0;
             */
-
             //fake data for offline demo
             demandVoltage[i][j] = Math.random();
             reportVoltage[i][j] = Math.random();
@@ -39,6 +50,11 @@ function fetchNewData(rows, cols, ODBkeys, demandVoltage, reportVoltage, reportC
             rampStatus[i][j] = Math.random();
             if(rampStatus[i][j] < 0.1) rampStatus[i][j] = 1;
             else rampStatus[i][j] = 0;
+        }
+    }
+
+    for(i=0; i<rows; i++){
+        for(j=0; j<cols; j++){
 
             //construct the parameter to be tested against the voltage alarm:
             testParameter = Math.abs(demandVoltage[i][j] - reportVoltage[i][j]); 
