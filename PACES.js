29c10438e5f085f6bd2047821ef49b7177e99cae
Bill Mills <mills.wj@gmail.com>
2013-02-19 13:41:57 -0800

    began implementing PACES tooltip

--- PACES.js
+++ PACES.js
@@ -1,9 +1,10 @@
-function PACES(monitor){
+function PACES(monitor, prefix, postfix){
 	this.monitorID = monitor;		        //div ID of wrapper div
 	this.canvasID = 'PACESCanvas'; 			//ID of canvas to draw top level TIGRESS view on
     this.linkWrapperID = 'SubsystemLinks';  //ID of div wrapping subsystem navigation links
     this.sidebarID = 'SubsystemSidebar';    //ID of right sidebar for this object
     this.topNavID = 'SubsystemsButton';     //ID of top level nav button
+    this.TTcanvasID = 'PACESTTCanvas';       //ID of hidden tooltip map canvas
 
     var that = this;
     //make a pointer at window level back to this object, so we can pass by reference to the nav button onclick
@@ -37,6 +38,26 @@ function PACES(monitor){
     this.canvas = document.getElementById(this.canvasID);
     this.context = this.canvas.getContext('2d');
 
+    //hidden Tooltip map layer
+    newCanvas = document.createElement('canvas');
+    newCanvas.setAttribute('id', this.TTcanvasID);
+    newCanvas.setAttribute('class', 'monitor');
+    newCanvas.setAttribute('style', 'top:' + ($('#SubsystemLinks').height() + 5)*1.25 +'px;')
+    newCanvas.setAttribute('width', this.canvasWidth);
+    newCanvas.setAttribute('height', this.canvasHeight);
+    document.getElementById(monitor).appendChild(newCanvas);
+    this.TTcanvas = document.getElementById(this.TTcanvasID);
+    this.TTcontext = this.TTcanvas.getContext('2d');
+    //Dirty trick to implement tooltip on obnoxious geometry: make another canvas of the same size hidden beneath, with the 
+    //detector drawn on it, but with each element filled in with rgba(0,0,n,1), where n is the channel number; fetching the color from the 
+    //hidden canvas at point x,y will then return the appropriate channel index.
+    //paint whole hidden canvas with R!=G!=B to trigger TT suppression:
+    this.TTcontext.fillStyle = 'rgba(50,100,150,1)';
+    this.TTcontext.fillRect(0,0,this.canvasWidth, this.canvasHeight);
+    //set up tooltip:
+    this.tooltip = new Tooltip(this.canvasID, 'PACESTipText', 'PACESttCanv', 'PACESTT', this.monitorID, prefix, postfix);
+    this.tooltip.obj = that;
+
     //drawing parameters
     this.centerX = this.canvasWidth/2;
     this.centerY = this.canvasHeight/2;
@@ -49,6 +70,7 @@ function PACES(monitor){
     	var i;
     	this.context.strokeStyle = '#999999'
 
+        //once for the display canvas....
     	for(i=0; i<5; i++){
     		this.context.save();
     		this.context.translate(this.centerX, this.centerY);
@@ -59,6 +81,44 @@ function PACES(monitor){
     		this.context.stroke();
     		this.context.restore();
     	}
+        //...and again for the tooltip encoding
+        for(i=0; i<5; i++){
+            this.TTcontext.fillStyle = 'rgba('+i+','+i+','+i+',1)';
+            this.TTcontext.save();
+            this.TTcontext.translate(this.centerX, this.centerY);
+            this.TTcontext.rotate(i*Math.PI*72/180);
+            this.TTcontext.beginPath();
+            this.TTcontext.arc(0, -this.arrayRadius, this.SiLiRadius, 0, 2*Math.PI);
+            this.TTcontext.closePath();
+            this.TTcontext.fill();
+            this.TTcontext.restore();
+        }
 
+    };
+
+    this.findCell = function(x, y){
+        var imageData = this.TTcontext.getImageData(x,y,1,1);
+        var index = -1;
+        if(imageData.data[0] == imageData.data[1] && imageData.data[0] == imageData.data[2]) index = imageData.data[0];
+        return index;
+    };
+
+    this.defineText = function(cell){
+        var toolTipContent = '<br>';
+        var nextLine;
+        var longestLine = 0;
+        var cardIndex;
+        var i;
+
+        nextLine = 'Channel '+cell;
+
+        //keep track of the longest line of text:
+        longestLine = Math.max(longestLine, this.tooltip.context.measureText(nextLine).width)
+        toolTipContent += nextLine;
+
+        document.getElementById(this.tooltip.ttTextID).innerHTML = toolTipContent;
+
+        //return length of longest line:
+        return longestLine;
     };
 }
